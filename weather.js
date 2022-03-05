1e44d75c5003c9b4599c193442645df5807c0ad2
Alif <redowan.alif@gmail.com>
2022-03-05 23:21:18 +0600

    first

--- weather.js
+++ weather.js
@@ -1,9 +1,17 @@
 const API_KEY = `8b9a876b5c4ad9f787574c9b4d599e07`
 const searchBtn =  () =>{
-    const searchResult = document.getElementById('searchCity').value;
-    const url = `http://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={searchResult}&appid={API_KEY}`
-    console.log(url);
-//     fetch(url)
-//     .then(res => res.json())
-//     .then(data => console.log(data))
+    const city = document.getElementById('city').value;
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayTemperature(data))
+}
+
+const setInnerText = (name, value) => {
+    document.getElementById(name).innerText = value
+}
+
+const displayTemperature = temperature => {
+    setInnerText('cityName' , temperature.name)
+    console.log(temperature);
 }
